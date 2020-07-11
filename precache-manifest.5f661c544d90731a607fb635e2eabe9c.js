550b3001eb0673090c0e428c6aecd3a665fd68de
vivek chaudhari <vivek11101997@gmail.com>
2020-07-11 11:55:43 +0530

    Updates

---
+++ precache-manifest.5f661c544d90731a607fb635e2eabe9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66bd6bf930637f1ea11df5880b799e7",
+    "url": "/news-app-client/index.html"
+  },
+  {
+    "revision": "4525defe7fe23deaad83",
+    "url": "/news-app-client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "757f27cd37263cc5e4a5",
+    "url": "/news-app-client/static/css/main.703c56ef.chunk.css"
+  },
+  {
+    "revision": "4525defe7fe23deaad83",
+    "url": "/news-app-client/static/js/2.02b54ac4.chunk.js"
+  },
+  {
+    "revision": "d341ac4facf083c92fe9249a5068ccc9",
+    "url": "/news-app-client/static/js/2.02b54ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757f27cd37263cc5e4a5",
+    "url": "/news-app-client/static/js/main.128374f8.chunk.js"
+  },
+  {
+    "revision": "e99c81f401013192b645",
+    "url": "/news-app-client/static/js/runtime-main.3e774afd.js"
+  },
+  {
+    "revision": "4221523770a424283cdd408e10c26b82",
+    "url": "/news-app-client/static/media/clear_all.42215237.svg"
+  },
+  {
+    "revision": "1f18ea43e8844787a3c29db697f02e51",
+    "url": "/news-app-client/static/media/empty.1f18ea43.svg"
+  },
+  {
+    "revision": "264cde8bda2768526a99799504c573cc",
+    "url": "/news-app-client/static/media/login.264cde8b.svg"
+  },
+  {
+    "revision": "ea8fc4981f789a7841e00d5aad322420",
+    "url": "/news-app-client/static/media/newspaper.ea8fc498.svg"
+  }
+]);
